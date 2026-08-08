f6608cc9374dbb82e7abdbab3d01450f1b02c9f0
Mukhtar-56 <mukhtrghavisi192661@gmail.com>
2026-08-08 10:40:31 +0500

    Data base created

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///scholarships.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+class Scholarship(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(200), nullable=False)
+
+    country = db.Column(db.String(100), nullable=False)
+
+    university = db.Column(db.String(200))
+
+    degree = db.Column(db.String(100))
+
+    deadline = db.Column(db.String(50))
+
+    description = db.Column(db.Text)
+
+    benefits = db.Column(db.Text)
+
+    eligibility = db.Column(db.Text)
+
+    documents = db.Column(db.Text)
+
+    apply_link = db.Column(db.String(500))
 
 @app.route("/")
 def home():
